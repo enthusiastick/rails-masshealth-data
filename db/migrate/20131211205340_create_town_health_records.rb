2c3f085bc71e2e3811a2c816a5626117776a95b3
Eben <eben.lowe@gmail.com>
2013-12-12 09:56:39 -0500

    Renamed columns in schema and added NA to nil step for sanitzation method.

--- db/migrate/20131211205340_create_town_health_records.rb
+++ db/migrate/20131211205340_create_town_health_records.rb
@@ -2,20 +2,20 @@ class CreateTownHealthRecords < ActiveRecord::Migration
   def change
     create_table :town_health_records do |t|
       t.string :town
-      t.integer :total_pop_year_2005
-      t.integer :age_0_to_19_year_2005
-      t.integer :age_65_plus_year_2005
-      t.integer :per_capita_income_year_2000
-      t.integer :persons_living_below_twox_poverty_year_2000
-      t.float :persons_living_below_twox_poverty_level_as_percent_year_2000
+      t.integer :total_pop
+      t.integer :age_0_to_19
+      t.integer :age_65_plus
+      t.integer :per_capita_income
+      t.integer :persons_living_below_twox_poverty
+      t.float :persons_living_below_twox_poverty_level_as_percent
       t.float :adequacy_prenatal_care
-      t.float :c_section_deliveries_2005_to_2008
-      t.integer :infant_deaths_2005_to_2008
-      t.float :infant_mortality_rate_2005_to_2008
-      t.float :low_birthweight_2005_to_2008
-      t.float :multiple_births_2005_to_2008
-      t.float :publicly_financed_prenatal_care_2005_to_2008
-      t.float :teen_births_2005_to_2008
+      t.float :c_section_deliveries
+      t.integer :infant_deaths
+      t.float :infant_mortality_rate
+      t.float :low_birthweight
+      t.float :multiple_births
+      t.float :publicly_financed_prenatal_care
+      t.float :teen_births
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,35 +9,41 @@
 require 'csv'
 
 def expurgate_integer(int)
-  unless int == nil
+  if int == 'NA'
+    nil
+  elsif int != nil
     int.strip.delete('$').delete(',').to_i
   end
 end
 
 def expurgate_float(flo)
-  unless flo == nil
+  if flo == 'NA'
+    nil
+  elsif flo != nil
     flo.strip.delete('$').delete(',').to_f
   end
 end
 
+ignore_strings = ['Note: for infant mortality rate (IMR), data suppressed where number of births < 500; for other variables, data suppressed where number of observations 1-4.', 'Massachusetts Total']
+
 CSV.foreach(Rails.root.join('db/data/mass_health_data.csv'), headers:true) do |row|
-  unless row['Geography'] == nil
+  unless row['Geography'] == nil || row['Geography'] == ignore_strings[0] || row['Geography'] == ignore_strings[1]
     town = TownHealthRecords.new
     town.town = (row['Geography'])
-    town.total_pop_year_2005 = expurgate_integer(row["total pop, year 2005"])
-    town.age_0_to_19_year_2005 = expurgate_integer(row["age 0-19, year 2005"])
-    town.age_65_plus_year_2005 = expurgate_integer(row["age 65+, year 2005"])
-    town.per_capita_income_year_2000 = expurgate_integer(row["Per Capita Income, year 2000"])
-    town.persons_living_below_twox_poverty_year_2000 = expurgate_integer(row["Persons Living Below 200% Poverty, year 2000 "])
-    town.persons_living_below_twox_poverty_level_as_percent_year_2000 = expurgate_float(row["% all Persons Living Below 200% Poverty Level, year 2000"])
+    town.total_pop = expurgate_integer(row["total pop, year 2005"])
+    town.age_0_to_19 = expurgate_integer(row["age 0-19, year 2005"])
+    town.age_65_plus = expurgate_integer(row["age 65+, year 2005"])
+    town.per_capita_income = expurgate_integer(row["Per Capita Income, year 2000"])
+    town.persons_living_below_twox_poverty = expurgate_integer(row["Persons Living Below 200% Poverty, year 2000 "])
+    town.persons_living_below_twox_poverty_level_as_percent = expurgate_float(row["% all Persons Living Below 200% Poverty Level, year 2000"])
     town.adequacy_prenatal_care = expurgate_float(row['% adequacy prenatal care (kotelchuck)'])
-    town.c_section_deliveries_2005_to_2008 = expurgate_float(row["% C-section deliveries, 2005-2008"])
-    town.infant_deaths_2005_to_2008 = expurgate_integer(row["Number of infant deaths, 2005-2008"])
-    town.infant_mortality_rate_2005_to_2008 = expurgate_float(row["Infant mortality rate (deaths per 1000 live births), 2005-2008"])
-    town.low_birthweight_2005_to_2008 = expurgate_float(row['% low birthweight 2005-2008'])
-    town.multiple_births_2005_to_2008 = expurgate_float(row["% multiple births, 2005-2008"])
-    town.publicly_financed_prenatal_care_2005_to_2008 = expurgate_float(row["% publicly financed prenatal care, 2005-2008"])
-    town.teen_births_2005_to_2008 = expurgate_float(row["% teen births, 2005-2008"])
+    town.c_section_deliveries = expurgate_float(row["% C-section deliveries, 2005-2008"])
+    town.infant_deaths = expurgate_integer(row["Number of infant deaths, 2005-2008"])
+    town.infant_mortality_rate = expurgate_float(row["Infant mortality rate (deaths per 1000 live births), 2005-2008"])
+    town.low_birthweight = expurgate_float(row['% low birthweight 2005-2008'])
+    town.multiple_births = expurgate_float(row["% multiple births, 2005-2008"])
+    town.publicly_financed_prenatal_care = expurgate_float(row["% publicly financed prenatal care, 2005-2008"])
+    town.teen_births = expurgate_float(row["% teen births, 2005-2008"])
     town.save
   end
 end
